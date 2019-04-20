1b1ce5aec9175398a02c4b0362e2ecafc302df3d
Rob Jones <rob@mojosix.co.uk>
2019-04-20 08:19:12 +0100

    setup

--- src/bottles.js
+++ src/bottles.js
@@ -1,3 +1,12 @@
-export const verse = () => {};
-export const verses = () => {};
-export const song = () => {};
+export const verse = n => {
+  return `verse ${n}`;
+};
+
+export const verses = (start = 99, end = 0) => {
+  return Array.from(Array(start + 1).keys())
+    .slice(end)
+    .reverse()
+    .map(verse);
+};
+
+export const song = () => verses();
