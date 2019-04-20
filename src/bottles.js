c69e179439c6a70049a9c3122b6ab4c2180be508
Rob Jones <rob@mojosix.co.uk>
2019-04-20 09:48:12 +0100

    join array

--- src/bottles.js
+++ src/bottles.js
@@ -6,7 +6,8 @@ export const verses = (start = 99, end = 0) => {
   return Array.from(Array(start + 1).keys())
     .slice(end)
     .reverse()
-    .map(verse);
+    .map(verse)
+    .join("\n");
 };
 
 export const song = () => verses();
