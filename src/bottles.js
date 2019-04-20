955f1e01654f86ca9706e3e1f358f1d70ddb2a84
Rob Jones <rob@mojosix.co.uk>
2019-04-20 07:36:31 +0100

    bottles reset

--- src/bottles.js
+++ src/bottles.js
@@ -1,8 +1,3 @@
-export const verse = () => {
-  return (
-    `99 bottles of beer on the wall, 99 bottles of beer.\n` +
-    `Take one down and pass it around, 98 bottles of beer on the wall.\n`
-  );
-};
+export const verse = () => {};
 export const verses = () => {};
 export const song = () => {};
