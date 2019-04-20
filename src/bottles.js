98b552f042cd212ecfc7c4d8a2dd65d4577349b5
Rob Jones <rob@mojosix.co.uk>
2019-04-20 09:47:09 +0100

    shameful green

--- src/bottles.js
+++ src/bottles.js
@@ -1,12 +1,38 @@
-export const verse = n => {
-  return `verse ${n}`;
+export const verse = number => {
+  switch (number) {
+    case 0:
+      return (
+        `No more bottles of beer on the wall, no more bottles of beer.\n` +
+        `Go to the store and buy some more, 99 bottles of beer on the wall.\n`
+      );
+      break;
+    case 1:
+      return (
+        `1 bottle of beer on the wall, 1 bottle of beer.\n` +
+        `Take it down and pass it around, no more bottles of beer on the wall.\n`
+      );
+      break;
+    case 2:
+      return (
+        `2 bottles of beer on the wall, 2 bottles of beer.\n` +
+        `Take one down and pass it around, 1 bottle of beer on the wall.\n`
+      );
+      break;
+    default:
+      return (
+        `${number} bottles of beer on the wall, ${number} bottles of beer.\n` +
+        `Take one down and pass it around, ${number -
+          1} bottles of beer on the wall.\n`
+      );
+  }
 };
 
 export const verses = (start = 99, end = 0) => {
   return Array.from(Array(start + 1).keys())
     .slice(end)
     .reverse()
-    .map(verse);
+    .map(verse)
+    .join("\n");
 };
 
 export const song = () => verses();
