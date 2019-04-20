60196f9ade03b281aa26a7f4bca60d3ee0924fdc
Rob Jones <rob@mojosix.co.uk>
2019-04-20 07:32:39 +0100

    initial commit

---
+++ src/bottles.js
@@ -0,0 +1,8 @@
+export const verse = () => {
+  return (
+    `99 bottles of beer on the wall, 99 bottles of beer.\n` +
+    `Take one down and pass it around, 98 bottles of beer on the wall.\n`
+  );
+};
+export const verses = () => {};
+export const song = () => {};
